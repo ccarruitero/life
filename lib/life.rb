af324d03c7957d2214720a84e14a5d15e3c4a6f9
César Carruitero <ccarruitero@protonmail.com>
2020-03-03 15:48:23 -0500

    init project

---
+++ lib/life.rb
@@ -0,0 +1,6 @@
+require "life/version"
+
+module Life
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/life_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Life do
+  it "has a version number" do
+    expect(Life::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
